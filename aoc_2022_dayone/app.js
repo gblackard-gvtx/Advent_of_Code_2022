66323c7c1f46552a599553e3b1abeca658485f81
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2022-12-02 10:58:47 -0600

    Day one finished.

---
+++ aoc_2022_dayone/app.js
@@ -0,0 +1,28 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+const readFileGetMax = (fileName)=>{
+    let content = readFileSync(fileName,'utf-8');
+    const arr = content.split(/\r?\n/);
+    let max = 0;
+    let total = 0;
+    let totalArr=[];
+
+    arr.forEach(element => {
+        if(element===''){
+            totalArr.push(total);
+            if (total>max) {
+                max = total
+            }
+          total=0; 
+        }else{
+            total+=parseInt(element)
+        }
+    });
+    totalArr.sort();
+    totalArr.reverse();
+    
+   let totalTop = totalArr[0]+totalArr[1]+totalArr[2];
+   console.log("Three Highest Proformers Total: "+ totalTop);
+    
+}
+const file = './calories.txt'
+readFileGetMax(file);
